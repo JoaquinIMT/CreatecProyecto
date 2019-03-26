d708b71c9d6b56490d6f85f320e68935d5283aad
JoaquinIMT <A01740395@itesm.mx>
2019-03-26 16:05:29 -0700

    Tenemos una nueva pantalla completa de los datos del cliente y creamos otra que falta trabajar para la información del consumo

--- app/src/main/java/com/example/createc/ClienteDatos.kt
+++ app/src/main/java/com/example/createc/ClienteDatos.kt
@@ -1,12 +1,27 @@
 package com.example.createc
 
+import android.content.Intent
+import android.icu.text.IDNA
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ClienteDatos : AppCompatActivity() {
+
+    lateinit var next2: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cliente_datos)
+
+        next2 = findViewById(R.id.next2)
+
+        next2.setOnClickListener {
+
+            val intent = Intent(applicationContext,InformacionConsumo::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
